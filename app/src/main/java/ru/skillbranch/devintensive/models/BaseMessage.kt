77e5c108c7c4baf6affcdb173d7a02c49bf0a843
niknet <niknet@email.com>
2019-07-11 20:36:51 +0300

    fix BaseMessage 3

--- app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
@@ -19,7 +19,7 @@ abstract class BaseMessage (
 
     abstract  fun formatMessage(): String
     companion object AbstractFactory{
-        private var lastId = -1
+        var lastId = -1
         fun makeMessage(
             from: User?,
             chat: Chat,
