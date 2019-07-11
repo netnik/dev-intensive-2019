8deedb7b26140dc0e6f67565b9351295877c4b35
niknet <niknet@email.com>
2019-07-11 23:05:47 +0300

    fix 7 User-> {lastVisit}, lastId->private, UserView -> private id

--- app/src/main/java/ru/skillbranch/devintensive/models/UserView.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/UserView.kt
@@ -6,7 +6,7 @@ package ru.skillbranch.devintensive.models
  **/
 
 class UserView(
-    val id: String,
+    private val id: String,
     val fullName: String,
     val nickName: String,
     var avatar: String? = null,
