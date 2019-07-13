e829d535a6f4a99357996763cae08ab66f40caf2
niknet <niknet@email.com>
2019-07-13 23:18:55 +0300

    fix BaseMessage 6

--- app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
@@ -18,21 +18,13 @@ abstract class BaseMessage (
 ) {
 
     abstract  fun formatMessage(): String
-    companion object AbstractFactory{
-        private var lastId = -1
-        fun makeMessage(
-            from: User?,
-            chat: Chat,
-            date: Date,
-            type: String = "text",
-            payload: Any?,
-            isIncoming: Boolean = false
-        ): BaseMessage {
+    companion object AbstractFactory {
+        private var lastId: Int = -1
+        fun makeMessage( from: User?, chat: Chat, date: Date, type: String = "text", payload: Any?, isIncoming: Boolean = false ) : BaseMessage {
             lastId++
             return when(type) {
-                "image"-> ImageMessage("$lastId", from, chat, date = date, image = payload as String, isIncoming = isIncoming)
-                else -> TextMessage("$lastId", from, chat, date = date, text = payload as String, isIncoming = isIncoming)
-
+                "image" -> ImageMessage(id = "$lastId", from = from, chat = chat, date = date, image = payload as String, isIncoming = isIncoming)
+                else -> TextMessage(id = "$lastId", from = from, chat = chat, date = date, text = payload as String, isIncoming = isIncoming)
             }
         }
     }
